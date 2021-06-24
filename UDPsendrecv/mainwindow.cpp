99af1bf480de24970ed88cc2633e8c3c7cc3a9be
aydinguler <69461459+aydinguler@users.noreply.github.com>
2021-06-24 16:45:27 +0300

    Add new conditions for Unicast Multicast selector comboBox

--- UDPsendrecv/mainwindow.cpp
+++ UDPsendrecv/mainwindow.cpp
@@ -1,24 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    socket = new QUdpSocket();
-    socket->bind(QHostAddress::LocalHost,1234);//127.0.0.1
-    //Unicast
-    //quint16 nport = 3956;
-    //QString strIP = "127.0.0.1";
-    //m_qudpSocket->bind(QHostAddress(strIP),nport);//Bind the local receiver IP and port (for unicast reception)
-    //connect(m_qudpSocket,SIGNAL(readyRead()),this,SLOT(RecvData()));
-    //Multicast
-    //m_qudpSocket->bind(QHostAddress::AnyIPv4,3956,QUdpSocket::ShareAddress);//Bind all ips to receive multicast group information
-    //m_qudpSocket->joinMulticastGroup(QHostAddress("224.0.0.10"));//Join the multicast group ip: 224.0.0.10
-    //m_qudpSocket->setSocketOption(QAbstractSocket::ReceiveBufferSizeSocketOption,1024*1024*8);//Set buffer
-    //connect(m_qudpSocket,SIGNAL(readyRead()),this,SLOT(RecvData()));//Connect the receiving signal slot
+    senderSocket = new QUdpSocket(this);
+    socket = new QUdpSocket(this);
 
     QAudioFormat format;//Define the type of audio processing
     format.setSampleRate(16000);//The acquisition frequency is 1s 16000 times
@@ -28,15 +17,7 @@ MainWindow::MainWindow(QWidget *parent)
     format.setSampleType(QAudioFormat::SignedInt);
     format.setByteOrder(QAudioFormat::LittleEndian);//Set the data type of Xiaowei
     input = new QAudioInput(format,this);
-    //inputDevice = input->start();//input starts to read the input audio signal and writes it into QIODevice, here is inputDevice
     output = new QAudioOutput(format,this);
-    //outputDevice = output->start();//Start playing
-    timer = new QTimer(this);
-
-    //connect(inputDevice,SIGNAL(readyRead()),this,SLOT(onReadyRead()));
-    //Slot function, when inputDevice receives the audio data written by input, it calls the onReadyRead function to send the data to the target host
-    //connect(inputDevice,SIGNAL(readyRead()),this,SLOT(readyRead()));
-    //Slot function, when outputDevice receives the audio data written by output, it calls the readyRead function
 }
 MainWindow::~MainWindow()
 {
@@ -51,12 +32,14 @@ void MainWindow::readyRead()
     quint16 senderPort;
     socket->readDatagram((char*)&ap,sizeof(ap),&sender,&senderPort);
     outputDevice->write(ap.audioDataRecv,ap.lensRecv);
-    //ui->textBrowser->setPlainText(sender.toString()+"\n"+senderPort+"\n"+ap.lensRecv+"\n"+ap.audioDataRecv+"\n");
-    ui->textBrowser->setPlainText(ap.audioDataRecv);
-    qDebug() << "Message from: " << sender.toString();
-    qDebug() << "Message port: " << senderPort;
-    qDebug() << "Message size: " << ap.lensRecv;
-    qDebug() << "Message data: " << ap.audioDataRecv;
+    ui->textBrowser->setPlainText("Message from: "+sender.toString()+"\n"+
+                                  "Message port: "+QString::number(senderPort)+"\n"+
+                                  "Message size:"+QString::number(ap.lensRecv)+"\n"+
+                                  "Message data:"+ap.audioDataRecv+"\n");
+    //qDebug() << "Message from: " << sender.toString();
+    //qDebug() << "Message port: " << QString::number(senderPort);
+    //qDebug() << "Message size: " << QString::number(ap.lensRecv);
+    //qDebug() << "Message data: " << ap.audioDataRecv;
 }
 void MainWindow::onReadyRead()
 {
@@ -66,27 +49,69 @@ void MainWindow::onReadyRead()
     ap.lensSend = inputDevice->read(ap.audioDataSend,1280);//Read audio
     //qDebug() << ap.lensSend;
     ui->textBrowser->setPlainText(ap.audioDataSend);
-    socket->writeDatagram((const char*)&ap,sizeof(ap),QHostAddress::LocalHost,1234);
-    //Send this structure to the target host, the port is 1234, and the IP is 127.0.0.1
+    senderSocket->writeDatagram((const char*)&ap,sizeof(ap),*targetAddress,*targetPort);
+    //Send this structure to the target host, the port and the IP are declared in the if statements
 }
 void MainWindow::on_pushButton_clicked(bool checked)
 {
-    inputDevice = input->start();
-    timer->stop();
-    if (ui->comboBox->currentText() == "Sender" && ui->pushButton->isChecked()==true){
-        connect(inputDevice,SIGNAL(readyRead()),this,SLOT(onReadyRead()));
-        ui->pushButton->setText("Stop");
+    if (ui->comboBox->currentText() == "Sender"
+            && (ui->comboBox_2->currentText() == "Unicast")
+            && ui->pushButton->isChecked()==true)
+    {
+        targetAddress = new QHostAddress("192.168.1.35");
+        targetPort = new quint16(45000);
+        inputDevice = input->start();//input starts to read the input audio signal and writes it into QIODevice, here is inputDevice
+        connect(inputDevice,SIGNAL(readyRead()),this,SLOT(onReadyRead()));//Slot function, when inputDevice receives the audio data written by input,
+        ui->pushButton->setText("Stop");                                  //it calls the onReadyRead function to send the data to the target host
     }
-    else if (ui->comboBox->currentText() == "Receiver" && ui->pushButton->isChecked()==true){
+    else if (ui->comboBox->currentText() == "Sender"
+             && (ui->comboBox_2->currentText() == "Multicast")
+             && ui->pushButton->isChecked()==true)
+    {
+        targetAddress = new QHostAddress("224.0.0.2");
+        targetPort = new quint16(9999);
+        inputDevice = input->start();//input starts to read the input audio signal and writes it into QIODevice, here is inputDevice
+        connect(inputDevice,SIGNAL(readyRead()),this,SLOT(onReadyRead()));//Slot function, when inputDevice receives the audio data written by input,
+        ui->pushButton->setText("Stop");                                  //it calls the onReadyRead function to send the data to the target host
+    }
+    else if (ui->comboBox->currentText() == "Receiver"
+             && ui->comboBox_2->currentText() == "Unicast"
+             && ui->pushButton->isChecked()==true)
+    {
         outputDevice = output->start();//Start playing
-        timer->start();
-        connect(timer,SIGNAL(timeout()),this,SLOT(readyRead()));
-        ui->pushButton->setText("Stop");
+        socket->bind(QHostAddress::AnyIPv4,45000);
+        connect(socket,SIGNAL(readyRead()),this,SLOT(readyRead()));//Slot function, when socket has data sent by onReadyRead function,
+        ui->pushButton->setText("Stop");                           //it calls the readyRead function to receive the data
     }
-    else{
+    else if (ui->comboBox->currentText() == "Receiver"
+             && ui->comboBox_2->currentText() == "Multicast"
+             && ui->pushButton->isChecked()==true)
+    {
+        outputDevice = output->start();//Start playing
+        socket->bind(QHostAddress(QHostAddress::AnyIPv4),9999,QUdpSocket::ReuseAddressHint|QUdpSocket::ShareAddress);
+        socket->joinMulticastGroup(QHostAddress("224.0.0.2"));//Join the multicast group ip: 224.0.0.2
+        connect(socket,SIGNAL(readyRead()),this,SLOT(readyRead()));//Slot function, when socket has data sent by onReadyRead function,
+        ui->pushButton->setText("Stop");                           //it calls the readyRead function to receive the data
+    }
+    else if (ui->comboBox->currentText() == "Receiver"
+             && ui->comboBox_2->currentText() == "Multicast"
+             && ui->pushButton->isChecked()==false)
+    {
+        socket->leaveMulticastGroup(QHostAddress("224.0.0.2"));//Leave the multicast group ip: 224.0.0.2
+        socket->close();
+        senderSocket->close();
+        output->stop();
+        input->stop();
         ui->pushButton->setChecked(false);
         ui->pushButton->setText("Start");
-        timer->stop();
+    }
+    else{
+        socket->close();
+        senderSocket->close();
+        output->stop();
+        input->stop();
+        ui->pushButton->setChecked(false);
+        ui->pushButton->setText("Start");
     }
     qDebug() << checked;
 }
